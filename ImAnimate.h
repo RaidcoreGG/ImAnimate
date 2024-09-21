1aa2a44408846540b6256ee09d4093c6b65c8190
Delta <46466697+DeltaGW2@users.noreply.github.com>
2024-09-21 22:24:56 +0200

    remove public declarations for curves

--- ImAnimate.h
+++ ImAnimate.h
@@ -44,36 +44,6 @@ namespace ImAnimate
 		OutCirc,
 		InOutCirc
 	};
-
-	float CurveFunc_Linear(float aProgress);
-
-	float CurveFunc_InSine(float aProgress);
-	float CurveFunc_OutSine(float aProgress);
-	float CurveFunc_InOutSine(float aProgress);
-
-	float CurveFunc_InQuad(float aProgress);
-	float CurveFunc_OutQuad(float aProgress);
-	float CurveFunc_InOutQuad(float aProgress);
-
-	float CurveFunc_InCubic(float aProgress);
-	float CurveFunc_OutCubic(float aProgress);
-	float CurveFunc_InOutCubic(float aProgress);
-
-	float CurveFunc_InQuart(float aProgress);
-	float CurveFunc_OutQuart(float aProgress);
-	float CurveFunc_InOutQuart(float aProgress);
-
-	float CurveFunc_InQuint(float aProgress);
-	float CurveFunc_OutQuint(float aProgress);
-	float CurveFunc_InOutQuint(float aProgress);
-
-	float CurveFunc_InExpo(float aProgress);
-	float CurveFunc_OutExpo(float aProgress);
-	float CurveFunc_InOutExpo(float aProgress);
-
-	float CurveFunc_InCirc(float aProgress);
-	float CurveFunc_OutCirc(float aProgress);
-	float CurveFunc_InOutCirc(float aProgress);
 }
 
 ///----------------------------------------------------------------------------------------------------
